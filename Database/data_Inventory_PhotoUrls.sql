04d02047b6caed46c995738434b304ebd85e887d
chini007 <srinivasan.sabapathy@softwareag.com>
2021-12-10 12:03:34 +0100

    Update

---
+++ Database/data_Inventory_PhotoUrls.sql
@@ -0,0 +1,15 @@
+INSERT INTO "PetShop_EEBoys"."PetShop_EEBoys.photoUrls" (
+"id", "urls") VALUES (
+'1'::bigint, 'https://cdn.vox-cdn.com/thumbor/KyRSPWoF3KfbCMH2xpevf1pshoA=/0x0:2560x1536/1220x813/filters:focal(1076x564:1484x972):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69113629/fake_cats.0.jpg'::text);
+
+INSERT INTO "PetShop_EEBoys"."PetShop_EEBoys.photoUrls" (
+"id", "urls") VALUES (
+'2'::bigint, 'https://cdn.vox-cdn.com/thumbor/KyRSPWoF3KfbCMH2xpevf1pshoA=/0x0:2560x1536/1220x813/filters:focal(1076x564:1484x972):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69113629/fake_cats.0.jpg'::text);
+
+INSERT INTO "PetShop_EEBoys"."PetShop_EEBoys.photoUrls" (
+"id", "urls") VALUES (
+'1'::bigint, 'https://cdn.vox-cdn.com/thumbor/KyRSPWoF3KfbCMH2xpevf1pshoA=/0x0:2560x1536/1220x813/filters:focal(1076x564:1484x972):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69113629/fake_cats.0.jpg'::text);
+
+INSERT INTO "PetShop_EEBoys"."PetShop_EEBoys.photoUrls" (
+"id", "urls") VALUES (
+'2'::bigint, 'https://cdn.vox-cdn.com/thumbor/KyRSPWoF3KfbCMH2xpevf1pshoA=/0x0:2560x1536/1220x813/filters:focal(1076x564:1484x972):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69113629/fake_cats.0.jpg'::text);
---
+++ Database/data_Inventory_tags.sql
@@ -0,0 +1,8 @@
+INSERT INTO "PetShop_EEBoys"."PetShop_EEBoys.photoUrls" (
+"id", "name") VALUES (
+'1'::bigint, 'Ugly'::text);
+
+INSERT INTO "PetShop_EEBoys"."PetShop_EEBoys.photoUrls" (
+"id", "name") VALUES (
+'2'::bigint, 'Awww'::text);
+
